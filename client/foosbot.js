cb911bc7a8f33911ea1ca0b810ed8433f46b78e0
Alex Nisnevich <alex.nisnevich@gmail.com>
2012-06-26 21:14:44 -0700

    client-side JS, first draft

---
+++ client/foosbot.js
@@ -0,0 +1,33 @@
+
+var match = [];
+var current_game;
+var score_limit = 5;
+
+function new_game(players) {
+  current_game = {
+    goals: [],
+    arrangement: players,
+    scores: [0, 0]
+  }
+}
+
+function score(player) {
+  var goal = {
+    'time': new Date(),
+    'scorer': player,
+    'arrangement': current_arrangement
+  }
+  current_game.goals.push(goal);
+
+  scoring_team = (current_game.arrangement[0].indexOf(player) != -1) ? 0 : 1;
+  current_game.scores[scoring_team]++;
+
+  if (current_game.scores[0] == score_limit || current_game.scores[1] == score_limit) {
+    match.push(current_game);
+    new_game();
+  }
+}
+
+function reposition(new_arrangement) {
+  current_arrangement = new_arrangement;
+}
